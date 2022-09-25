5ca371f49f943c3afa8b1ff99bb02225dfb54d9a
EsteBug11 <109556111+EsteBug11@users.noreply.github.com>
2022-09-25 15:02:16 +0000

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,6 +1,6 @@
-#create simple program for scan 1024 ports in ip address and show open ports
+#This tool Is for  basic scan for 65535 ports
 import socket,sys,threading,queue,time,os,subprocess,platform,socket,threading,queue,time,os,subprocess,platform
-
+#scan function for ip and port
 def scan(ip,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.5)
@@ -10,7 +10,7 @@ def scan(ip,port):
         s.close()
     except:
         pass
-
+#function main for scan ,take input ip
 def main():
     ip = input("Enter ip address: ")
     for port in range(1,65535):
